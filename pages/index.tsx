c8b382baf24a44e046a3fb953fdf132dafcf5e22
Tekiter <tekiter8@gmail.com>
2020-12-03 14:12:36 +0000

    Change to scss and add animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,23 @@
+import { gsap } from "gsap";
+import { TextPlugin } from "gsap/dist/TextPlugin";
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import { useEffect, useRef } from "react";
+import styles from "../styles/Home.module.scss";
+
+gsap.registerPlugin(TextPlugin);
+
+function MainText() {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const tl = gsap.timeline();
+    tl.from(ref.current, {y: "+=300", autoAlpha: 0, duration: 1});
+    tl.to(ref.current, {duration:1, text: "Hello,", ease: "none"});
+    tl.to(ref.current, {duration:1, text: "Hello, Tekiter world!", ease: "none"});
+  }, []);
+
+  return <h1 ref={ref} className={styles.title}>Hello, world!</h1>;
+}
 
 export default function Home() {
   return (
@@ -10,13 +28,8 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Hello, Tekiter world!
-        </h1>
-
-        <div className={styles.grid}>
-          
-        </div>
+        <MainText />
+        <div className={styles.grid}></div>
       </main>
     </div>
   );
