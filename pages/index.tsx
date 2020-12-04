ecccfdc5350202c577d98bcc1b021c7e75822e81
Tekiter <tekiter8@gmail.com>
2020-12-04 14:59:42 +0000

    Create link box

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,24 @@
 import { gsap } from "gsap";
 import { TextPlugin } from "gsap/dist/TextPlugin";
 import Head from "next/head";
-import { forwardRef, useEffect, useImperativeHandle, useRef } from "react";
-import { HelloTekiterWorld, HelloTekiterWorldRef } from "../components/intro";
+import { useEffect, useRef } from "react";
+import {
+  HelloTekiterWorld,
+  HelloTekiterWorldRef,
+  TekiterLinks,
+} from "../components/intro";
 import styles from "../styles/Home.module.scss";
 
 gsap.registerPlugin(TextPlugin);
 
 export default function Home() {
   const titleRef = useRef<HelloTekiterWorldRef>(null);
+  const linkRef = useRef(null);
 
   useEffect(() => {
     const tl = gsap.timeline();
     tl.add(titleRef.current.timeline());
+    tl.add(linkRef.current.timeline(), ">0.5");
   }, []);
 
   return (
@@ -24,6 +30,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <HelloTekiterWorld ref={titleRef} />
+        <TekiterLinks ref={linkRef} />
       </main>
     </div>
   );
