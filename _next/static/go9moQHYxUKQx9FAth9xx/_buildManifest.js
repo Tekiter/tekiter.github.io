cf5d6f7590f22ae950fdce9b5cc3d295db4ad9c9
Tekiter <tekiter8@gmail.com>
2021-05-30 07:35:57 +0000

    Update gh-pages to output generated at b8b667d

--- _next/static/go9moQHYxUKQx9FAth9xx/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-a44f1178bef61947028e.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-33746c4480fbb3618e21.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
